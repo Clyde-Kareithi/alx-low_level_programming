9134a768568d0f4611bc2e30f2c8a9105d9445c8
Clyde-Kareithi <klydedavid1@gmail.com>
2023-09-08 03:26:13 -0700

    No spaces

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{	
+{
 int digit;
 
 	for (digit = 0; digit <= 9; digit++)
@@ -17,7 +17,6 @@ int digit;
 	putchar(digit + '0');
 	}
 	putchar('\n');
-	
 	return (0);
 }
 
