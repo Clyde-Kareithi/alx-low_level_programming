ef44b72e0474f4061e768c700aa63d4ee8684736
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-24 08:55:25 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,19 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *current;
+listint_t *current, *temp;
 
 	if (head == NULL)
-	return ();  /* Handle NULL input */
+	return ();
 
-	while (*head != NULL)
-	{
 	current = *head;
-	*head = (*head)->next;
-	free(current);
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
+
+	*head = NULL;
 }
 
