a2b55f3997cfa68529369a76b450f62acef44d14
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-24 11:40:07 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,17 +11,30 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current = head;
 	size_t count = 0;
+	const listint_t *check = head;
 
 	while (current)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
+
 	if (current <= current->next)
 	{
 		printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-		exit(98);
+	break;
 	}
+
 	current = current->next;
+
+	if (current == check)
+	{
+	break;
+	}
+	}
+
+	if (current == check)
+	{
+	exit(98);
 	}
 
 	return (count);
