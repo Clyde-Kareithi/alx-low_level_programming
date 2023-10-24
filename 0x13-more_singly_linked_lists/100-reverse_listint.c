69a56e8eb33f79e9a6519a7cd75cf1722afcdb6a
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-24 11:08:56 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,14 +7,14 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL, *next, *current = *head;
+	listint_t *prev = NULL, *next;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
 	*head = prev;
