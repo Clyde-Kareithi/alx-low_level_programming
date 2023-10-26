9898870bb09b58693959be38b3184925a1ff4c4a
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-26 08:47:50 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary string to an unsigned int.
- * @b: A pointer to a binary string.
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: A pointer to a string containing 0 and 1 characters.
  *
- * Return: The converted unsigned int, or 0 on failure.
+ * Return: The converted unsigned integer, or 0 if any character in the
+ * string is not '0' or '1' or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
+
 {
 	unsigned int result = 0;
 
 	if (b == NULL)
-		return (0);
+	return (0);
 
 	while (*b)
 	{
-		if (*b != '0' && *b != '1')
-			return (0);
-
-		result = (result * 2) + (*b - '0');
+	if (*b == '0' || *b == '1')
+	{
+		result = (result << 1) | (*b - '0');
 		b++;
+	}
+	else
+	{
+	return (0);
+	}
 	}
 
 	return (result);
