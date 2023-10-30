aace4a8961a1ac7ba6525384136af113784cb3f1
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-30 08:36:03 -0700

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFSIZE 1024
+
 /**
  * main - Copy content from file_from to file_to.
  * @argc: Argument count.
@@ -12,57 +14,59 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t n_read, n_write;
-	char buffer[1024];
-
 	if (argc != 3)
-		{
+	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
-		}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-		}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-		}
-
-	while ((n_read = read(fd_from, buffer, 1024)) > 0)
-	{
-		n_write = write(fd_to, buffer, n_read);
-	if (n_write == -1)
-		{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-		}
 	}
 
-	if (n_read == -1)
+	if (copy_file(argv[1], argv[2]) != 0)
 	{
-	dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
 
-	if (close(fd_from) == -1)
+	return (0);
+}
+
+/**
+ * copy_file - Copy the content from file_from to file_to.
+ * @from: Source file.
+ * @to: Destination file.
+ * Return: 0 on success, -1 on failure.
+ */
+int copy_file(const char *from, const char *to)
+{
+	int fd_from, fd_to;
+	ssize_t n_read, n_write;
+	char buffer[BUFSIZE];
+
+	fd_from = open(from, O_RDONLY);
+	if (fd_from == -1)
+	return (-1);
+
+	fd_to = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		close(fd_from);
+		return (-1);
 	}
 
-	if (close(fd_to) == -1)
+	while ((n_read = read(fd_from, buffer, BUFSIZE)) > 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		n_write = write(fd_to, buffer, n_read);
+		if (n_write == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+		return (-1);
+		}
 	}
+
+	close(fd_from);
+	close(fd_to);
+
+	if (n_read == -1)
+	return (-1);
 
 	return (0);
 }
