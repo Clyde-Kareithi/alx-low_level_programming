3c894207a21db59b5c6cb9d596ddb667ef0b5daa
Clyde-Kareithi <klydedavid1@gmail.com>
2023-10-30 09:15:49 -0700

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,38 +40,27 @@ int main(int argc, char *argv[])
 		exit(99);
 		}
 
-	if (copy_file_contents(fd_from, fd_to, buffer) == -1)
-	exit(99);
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		{
-		dprintf(2, "Error: Can't close file descriptors\n");
-		exit(100);
-		}
-
-	return (0);
-}
-
-/**
- * copy_file_contents - Copy contents from one file descriptor to another.
- * @fd_from: Source file descriptor.
- * @fd_to: Destination file descriptor.
- * @buffer: Temporary buffer for copying.
- * Return: 0 on success, -1 on failure.
- */
-int copy_file_contents(int fd_from, int fd_to, char *buffer)
-{
-	ssize_t n_read, n_write;
-
-	while ((n_read = read(fd_from, buffer, BUFSIZE) > 0))
+	while ((n_read = read(fd_from, buffer, BUFSIZE)) > 0)
 	{
 		n_write = write(fd_to, buffer, n_read);
-	if (n_write == -1)
-	return (-1);
-}
+		if (n_write == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 
 	if (n_read == -1)
-	return (-1);
+		{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+		}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+	dprintf(2, "Error: Can't close file descriptors\n");
+	exit(100);
+	}
 
 	return (0);
 }
