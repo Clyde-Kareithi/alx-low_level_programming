baca4059b29b2bb166307e83ffea6516b142f92a
Clyde-Kareithi <klydedavid1@gmail.com>
2023-12-14 09:16:50 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,53 @@
 #include "main.h"
 
-unsigned int binary_to_uint(const char *b){
-unsigned int result = 0;
+/**
+ * power_of_two - calculates the power of 2
+ * @exp: exponent value
+ *
+ * Return: the result of 2 raised to the power of exp
+ */
+unsigned int power_of_two(int exp)
+{
+	if (exp == 0)
+	return (1);
 
-	if (b == NULL)
-	return 0;
-
-	while (*b)
+unsigned int result = 1;
+	while (exp > 0)
 	{
-	if (*b != '0' && *b != '1')
-	return 0;
-        
-	result = (result * 2) + (*b - '0');
-	b++;
+		result *= 2;
+		exp--;
+	}
+	return result;
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0;
+	unsigned int dec_val = 0;
+
+	if (!b)
+	return (0);
+
+
+	while (b[len])
+	{
+	if (b[len] != '0' && b[len] != '1')
+	return (0);
+	len++;
 	}
 
-return result;
+	for (int i = len - 1, exp = 0; i >= 0; i--, exp++)
+	{
+		if (b[i] == '1')
+		dec_val += power_of_two(exp);
+	}
+
+	return dec_val;
 }
 
