c59388020262e1d962eb5a8cd36381e85e4b04a6
Clyde-Kareithi <klydedavid1@gmail.com>
2023-12-14 09:25:49 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,53 +1,26 @@
 #include "main.h"
 
 /**
- * power_of_two - calculates the power of 2
- * @exp: exponent value
- *
- * Return: the result of 2 raised to the power of exp
- */
-unsigned int power_of_two(int exp)
-{
-	if (exp == 0)
-	return (1);
-
-unsigned int result = 1;
-	while (exp > 0)
-	{
-		result *= 2;
-		exp--;
-	}
-	return result;
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0;
+	int i;
 	unsigned int dec_val = 0;
 
 	if (!b)
-	return (0);
+		return (0);
 
-
-	while (b[len])
+	for (i = 0; b[i]; i++)
 	{
-	if (b[len] != '0' && b[len] != '1')
-	return (0);
-	len++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	for (int i = len - 1, exp = 0; i >= 0; i--, exp++)
-	{
-		if (b[i] == '1')
-		dec_val += power_of_two(exp);
-	}
-
-	return dec_val;
+	return (dec_val);
 }
 
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,28 @@
 #include "main.h"
 
-void print_binary(unsigned long int n){
-	int bit = sizeof(n) * 8 - 1;
-	int flag = 0;
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
 
-	if (n == 0){
-		_putchar('0');
-	return;
-	}
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
 
-	while (bit >= 0){
-	if ((n >> bit) & 1){
-		_putchar('1');
-		flag = 1;
-	}
-	else if (flag == 1 || bit == 0){
-		_putchar('0');
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
 		}
-	bit--;
+		else if (count)
+			_putchar('0');
 	}
+	if (!count)
+		_putchar('0');
 }
+
 
